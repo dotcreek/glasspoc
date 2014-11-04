e8e9093823a671318bde644938d299d9cd2f58f4
kevinalfa17 <kevinalfa17@gmail.com>
2014-11-04 11:55:28 -0600

    Optimized code

--- app/src/main/java/dotcreek/argumentedreality/CVActivity.java
+++ app/src/main/java/dotcreek/argumentedreality/CVActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.WindowManager;
@@ -17,12 +16,16 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDouble;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -33,17 +36,21 @@ import java.util.List;
 
 public class CVActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
 
+    /**     Variables    */
 
+    //region TAG del log
     private static final String TAG = "CV ArgumentedReality";
-    private final Handler mHandler = new Handler();
+    //endregion
 
-    /** Audio manager usado para reproducir efectos de sonido */
+    //region Controlador de audio
     private AudioManager mAudioManager;
+    //endregion
 
-    /** Detector de gestos para mostrar el menu */
+    //region Controlador del touchpad
     private GestureDetector mGestureDetector;
+    //endregion
 
-    /** OpenCV*/
+    //region Variables OpenCV
     private Mat mRGB;
     private Mat mGray;
     private Mat mThreshold;
@@ -51,6 +58,8 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
     private Mat mHierarchy;
     private Mat mCanonical;
     private Mat mIntrinsics;
+    private Mat rvec;
+    private Mat tvec;
     private MatOfDouble mDistortion;
     private MatOfPoint mTempContour;
     private MatOfPoint mPoints;
@@ -58,17 +67,32 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
     private MatOfPoint2f mApprox ;
     private CameraBridgeViewBase mOpenCvCameraView;
     private List<MatOfPoint> lstContours;
+    //endregion
 
-    /** Procesamiento de marcas */
+    //region Procesador de Marcas
     private MarkerProcessor mpProcesador;
+    //endregion+
+
+    //region Variables OpenGL
+
+    //endregion
+
+
+    /**     Funciones   */
+
+    //region Funciones OnCreate OnDestroy OnResume OnPause
 
     @Override
     protected void onCreate(Bundle bundle) {
+
         super.onCreate(bundle);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_cv);
 
+        /* Controlador de audio */
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        /* Controlador del touchpad  */
         mGestureDetector = createGestureDetector(this);
 
          /*Configuración del OpenCV*/
@@ -81,6 +105,7 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
 
     @Override
     protected void onResume() {
+        /*Se vuelven a cargar las librerias OpenCV*/
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
         super.onResume();
 
@@ -91,7 +116,10 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         super.onPause();
     }
 
-    /**      Funciones del GestureDetector       */
+    //endregion
+
+    //region Funciones del GestureDetector
+
     private GestureDetector createGestureDetector(Context context) {
 
         GestureDetector gestureDetector = new GestureDetector(context);
@@ -129,8 +157,6 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
             @Override
             public void onFingerCountChanged(int previousCount, int currentCount) {
 
-
-
             }
         });
         gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
@@ -144,7 +170,7 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         return gestureDetector;
     }
 
-    /** Funcion que envia los eventos del touchpad al GestureDetector */
+    /* Funcion que envia los eventos del touchpad al GestureDetector */
     @Override
     public boolean onGenericMotionEvent(MotionEvent event) {
         if (mGestureDetector != null) {
@@ -152,10 +178,11 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         }
         return false;
     }
+    //endregion
 
-    /**     Funciones OpenCV     */
+    //region Funciones de la camara OpenCV
 
-    /** Callback del OpenCV */
+    /* Callback del OpenCV */
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -173,11 +200,12 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         }
     };
 
-    /**     Inicia camara       */
+    /* Inicia camara */
     public void onCameraViewStarted(int width, int height) {
 
         Log.i(TAG, "width height" + width + " " + height);
 
+        /* Se obtienen los valores de calibración */
         CameraCalibrator mCalibrator = new CameraCalibrator(width, height);
         if (CalibrationResult.tryLoad(this, mCalibrator.getCameraMatrix(), mCalibrator.getDistortionCoefficients())) {
             mCalibrator.setCalibrated();
@@ -187,6 +215,7 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
             mCalibrator.getDistortionCoefficients().copyTo(mDistortion);
         }
 
+        /* Se crean las matrices */
         mRGB = new Mat(height, width, CvType.CV_8UC4);
         mGray = new Mat(height, width, CvType.CV_8UC1);
         mThreshold = new Mat();
@@ -197,7 +226,7 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
 
     }
 
-    /**     Detiene camara       */
+    /* Detiene camara */
     public void onCameraViewStopped() {
         mRGB.release();
         mGray.release();
@@ -205,7 +234,8 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         mBlur.release();
         mHierarchy.release();
     }
-    /**     Recibe frame        */
+
+    /*  Recibe frame */
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
         //Matrices usadas
@@ -217,8 +247,8 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         Imgproc.blur(mGray,mBlur,new Size(3, 3));
         Imgproc.threshold(mBlur,mThreshold,128.0,255.0,Imgproc.THRESH_OTSU);
 
-        //Se aplica el umbral para separar el cuadrado negro
-       // Imgproc.adaptiveThreshold(mGray,mThreshold,255,Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,Imgproc.THRESH_BINARY_INV,7,7);
+        /*//Se aplica el umbral para separar el cuadrado negro
+        Imgproc.adaptiveThreshold(mGray,mThreshold,255,Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,Imgproc.THRESH_BINARY_INV,7,7);*/
 
         //Se buscan los contornos
         lstContours = new ArrayList<MatOfPoint>();
@@ -248,15 +278,15 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
                     //if(mPoints.toArray().length==4 && Imgproc.isContourConvex(mPoints)){
 
 
-                   /** Se camba la perspectiva de la marca */
+                   /* Se camba la perspectiva de la marca */
                    mCanonical = mpProcesador.CambiarPerspectiva(mPoints,mGray);
 
 
-                    /** Se calcula el ID de la marca */
+                    /* Se calcula el ID de la marca */
                     int nRotations = 0;
                     int id = mpProcesador.CalcularID(mCanonical,nRotations);
 
-
+                    //Si la marca tiene un ID Valido
                     if (id != -1) {
                         escribirID(mRGB, mPoints, new Scalar(0, 0, 255), id);
                         //Se unen las cuatro esquinas para dibujar un cuadrado
@@ -264,22 +294,12 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
                         //escribirPuntos(mRGB,mPoints,new Scalar(0,0,255));
                     }
 
-                    //Se establecen los parametros 3D
-                    //MatOfPoint3f objectPoints = new MatOfPoint3f(new Point3(-1, -1, 0), new Point3(-1, 1, 0), new Point3(1, 1, 0), new Point3(1, -1, 0));
-                    //Mat rvec = new Mat();
-                    //Mat tvec = new Mat();
-                    //Calib3d.solvePnP(objectPoints, mApprox, mIntrinsics, mDistortion, rvec, tvec);
+                     /* Se establecen los parametros 3D */
+                    configuración3D();
 
-                    //Se dibuja una linea 3D
-                    //MatOfPoint3f linea3d = new MatOfPoint3f(new Point3(0, 0, 0), new Point3(0, 0, 1));
-                    //MatOfPoint2f linea2d = new MatOfPoint2f();
-                    //Calib3d.projectPoints(linea3d, rvec, tvec, mIntrinsics, mDistortion, linea2d);
-                    //Core.line(mRGB, linea2d.toArray()[0], linea2d.toArray()[1], new Scalar(0, 255, 0), 2);
-
-
-                    //Preview del marker
-                    //mCanonical.convertTo(mCanonical,mRGB.type());
-                    //mCanonical.copyTo(mGray.submat(new Rect(0, 0, 70, 70)));
+                    /*//Preview del marker
+                    mCanonical.convertTo(mCanonical,mRGB.type());
+                    mCanonical.copyTo(mGray.submat(new Rect(0, 0, 70, 70)));*/
 
                 }
             }
@@ -287,9 +307,12 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         return mRGB;
     }
 
-    /** Funcion que toma 4 puntos cardinales y crea lineas entre ellos para dibujar un cuadrado */
-    private void dibujarCuadrado(Mat imagen,MatOfPoint puntos,Scalar color){
+    //endregion
 
+    //region Funciones de realidad aumentada OpenCV
+
+    /* Funcion que toma 4 puntos cardinales y crea lineas entre ellos para dibujar un cuadrado */
+    private void dibujarCuadrado(Mat imagen,MatOfPoint puntos,Scalar color){
         Core.line(imagen, puntos.toArray()[0], puntos.toArray()[1], color,2);
         Core.line(imagen, puntos.toArray()[1], puntos.toArray()[2], color,2);
         Core.line(imagen, puntos.toArray()[2], puntos.toArray()[3], color,2);
@@ -297,6 +320,7 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
 
     }
 
+    /* Funcion que escribe en pantalla los puntos cardinales con su respectiva ubicacion */
     private void escribirPuntos(Mat imagen,MatOfPoint puntos,Scalar color){
         Core.putText(imagen, puntos.toArray()[0].toString(), puntos.toArray()[0], Core.FONT_ITALIC, 0.7, color, 2);
         Core.putText(imagen, puntos.toArray()[1].toString(), puntos.toArray()[1], Core.FONT_ITALIC, 0.7, color, 2);
@@ -304,15 +328,35 @@ public class CVActivity extends Activity implements CameraBridgeViewBase.CvCamer
         Core.putText(imagen, puntos.toArray()[3].toString(), puntos.toArray()[3], Core.FONT_ITALIC, 0.7, color, 2);
     }
 
+    /* Función que escribe el ID de un Marker */
     private void escribirID(Mat imagen,MatOfPoint puntos,Scalar color,int ID){
-        Core.putText(imagen, "ID: "+ID, puntos.toArray()[0], Core.FONT_ITALIC, 0.7, color, 2);
+
+        double x = (puntos.toArray()[0].x + puntos.toArray()[2].x)/2;
+        double y = (puntos.toArray()[0].y + puntos.toArray()[2].y)/2;
+
+        Core.putText(imagen, "ID: "+ID, new Point((int)x,(int)y), Core.FONT_ITALIC, 0.7, color, 2);
 
     }
 
+    /* Función que establece los parametros 3D*/
+    private void configuración3D(){
+        MatOfPoint3f objectPoints = new MatOfPoint3f(new Point3(-1, -1, 0), new Point3(-1, 1, 0), new Point3(1, 1, 0), new Point3(1, -1, 0));
+        rvec = new Mat();
+        tvec = new Mat();
+        Calib3d.solvePnP(objectPoints, mApprox, mIntrinsics, mDistortion, rvec, tvec);
 
+    }
 
+    /*Funcion que dibuja una linea en 3D*/
+    private void dibujarLinea(Mat imagen){
 
+        MatOfPoint3f linea3d = new MatOfPoint3f(new Point3(0, 0, 0), new Point3(0, 0, 1));
+        MatOfPoint2f linea2d = new MatOfPoint2f();
+        Calib3d.projectPoints(linea3d, rvec, tvec, mIntrinsics, mDistortion, linea2d);
+        Core.line(imagen, linea2d.toArray()[0], linea2d.toArray()[1], new Scalar(0, 255, 0), 2);
+    }
 
+    //endregion
 
 
 }
